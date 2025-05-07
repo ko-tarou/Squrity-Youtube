448e5da773093d860f6a6e367ed393c13803f50a
ko-tarou <tkota6174@gmail.com>
2025-05-07 18:46:28 +0900

    add フォルダ構成完成

---
+++ back/api/Alchemy/notice.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Notice(Base):
+    __tablename__ = "notice"
+
+    notice_id = Column(Integer, primary_key=True, index=True)
+    receive_user_id = Column(Integer, nullable=False)
